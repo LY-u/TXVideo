09732649bf93346c86030c87860b7429b97f296d
null <18868801547@163.com>
2020-07-06 18:25:16 +0800

    修改环境变量

--- src/assets/api.js
+++ src/assets/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 class Api {
 
   constructor(){
-    this.prePath = process.env.NODE_ENV === 'production' ?　'' : '/api'
+    let domain = 'http://vv.video.qq.com'
+    this.prePath = process.env.NODE_ENV === 'production' ?　domain : '/api'
   }
   
   async getInfo(id){
-    let domain = 'http://vv.video.qq.com'
     let path = this.prePath + `/getinfo?vids=${id}&platform=101001&charge=0&otype=json`
     // let path = domain + `/getinfo?vids=${id}&platform=101001&charge=0&otype=json`
     const res = await axios.get(path);
